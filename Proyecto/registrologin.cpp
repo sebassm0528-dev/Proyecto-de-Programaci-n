58506aa7276b51faf372c82821ec24c62dc6321c
Sebastian Mañay <sebassm0528@gmail.com>
2026-01-22 15:39:10 -0500

    Realización del diseño del registro de usuarios. Tambien la programación del registro y que se guarden los usuarios en un archivo .txt

---
+++ Proyecto/registrologin.cpp
@@ -0,0 +1,83 @@
+#include "registrologin.h"
+#include "./ui_registrologin.h"
+#include "mainwindow.h"
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
+
+
+registroLogin::registroLogin(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::registroLogin)
+{
+    ui->setupUi(this);
+}
+
+registroLogin::~registroLogin()
+{
+    delete ui;
+}
+
+//------------------------------------------------------------------------------------------------------------------
+
+void registroLogin::on_pushButton_clicked()
+{
+    QString usuario = ui->usuario_lineEdit->text();
+    QString password = ui->password_lineEdit->text();
+    QString confirmarP = ui->confPassw_lineEdit->text();
+
+//Valida que todos los campos esten llenos-------------------------------------------------------------
+    if(usuario.isEmpty() || password.isEmpty() || confirmarP.isEmpty()){
+        QMessageBox::warning(this,"Error","Complete todos los campos");
+        return;
+    }
+
+//Para validar que las contraseñas coincidan-----------------------------------------------------------
+    if(password != confirmarP){
+        QMessageBox::warning(this,"Error","Las contraseñas no coinciden");
+        return;
+    }
+
+//Para guardar en un archivo los usuarios registrados--------------------------------------------------
+    QString ruta = QCoreApplication::applicationDirPath() + "/usuarios.txt";
+
+    // Validar si un usuario esta duplicado--------------------------------------------
+    QFile file(ruta);
+    if(file.open(QIODevice::ReadOnly | QIODevice::Text)){
+        QTextStream in(&file);
+        while(!in.atEnd()){
+            QString linea = in.readLine();
+            QStringList datos = linea.split(";");
+
+            if(datos[0] == usuario){
+                QMessageBox::warning(this,"Registro","Usuario ya existe");
+                file.close();
+                return;
+            }
+        }
+        file.close();
+    }
+
+    //Pra guardar usuario registrado--------------------------------------------------
+    if(file.open(QIODevice::Append | QIODevice::Text)){
+        QTextStream out(&file);
+        out << usuario << ";" << password << "\n";
+        file.close();
+    }
+
+    QMessageBox::information(this,"Registro","Usuario registrado");
+
+    //Para volver al login------------------------------------------------------------
+    MainWindow *login = new MainWindow();
+    login->show();
+    this->close();
+}
+
+
+void registroLogin::on_pushButton_2_clicked()
+{
+    MainWindow *login = new MainWindow;
+    login->show();
+    this->close();
+}
+
---
+++ Proyecto/registrologin.h
@@ -0,0 +1,28 @@
+#ifndef REGISTROLOGIN_H
+#define REGISTROLOGIN_H
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class registroLogin;
+}
+QT_END_NAMESPACE
+
+class registroLogin : public QDialog
+{
+    Q_OBJECT
+
+public:
+    registroLogin(QWidget *parent = nullptr);
+    ~registroLogin();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+private:
+    Ui::registroLogin *ui;
+};
+#endif // REGISTROLOGIN_H
